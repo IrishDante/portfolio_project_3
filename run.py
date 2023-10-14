ede49b2a78390deaf08608f040c1670dd3bf28fa
Daniel Neville <dneville@tcd.ie>
2023-10-14 23:25:19 +0100

    added place ship on game board method to simplify place ship method further

--- run.py
+++ run.py
@@ -18,11 +18,12 @@ MISS = "*"
 class GameBoard:
     """Creates a game board object"""
     grid = []
+
     def __init__(self, board_dimension):
         """Initialises Game Board object based on dimensions provided taking board dimension allow dynamic boards"""
 
         self.board_dimension = board_dimension
-        self.grid = [0 for i in range(self.board_dimension * self.board_dimension)]
+        self.grid = [EMPTY for i in range(self.board_dimension * self.board_dimension)]
         print(self.grid)
 
     def print_game_board(self):
@@ -37,7 +38,6 @@ class GameBoard:
 
     def update_game_board(self, row, column, new_value):
         """Updates a value at a specific coordinate on the game board"""
-
         row -= 1
         if row <= 1:
             self.grid[row * self.board_dimension + column - 1] = new_value
@@ -56,14 +56,7 @@ class GameBoard:
         ship = self.order_ship_highest_last(ship)
         ship_length = ship[1][0] - ship[0][0] + ship[1][1] - ship[0][1]
         #  the matching pair negate so the difference between the unique values is found
-        if ship[0][1] != ship[0][0]:
-            while ship_length > 0:
-                self.update_game_board(ship[0][0], ship[0][1] - ship_length, "@")
-                ship_length -= 1
-        else:
-            while ship_length > 0:
-                self.update_game_board(ship[1][1] - ship_length, ship[1][0], "@")
-                ship_length -= 1
+        self.place_ship_on_game_board(ship, ship_length)
 
     @staticmethod
     def order_ship_highest_last(ship_to_examine):
@@ -76,6 +69,17 @@ class GameBoard:
             ship[0], ship[1] = ship[1], ship[0]
         return ship
 
+    def place_ship_on_game_board(self, ship, ship_length):
+
+        if ship[0][0] != ship[0][1]:  # check if ship columns are different
+            while ship_length > 0:
+                self.update_game_board(ship[0][0], ship[0][1] - ship_length, SHIP)
+                ship_length -= 1
+        else:  # applies if rows are different
+            while ship_length > 0:
+                self.update_game_board(ship[1][1] - ship_length, ship[1][0], SHIP)
+                ship_length -= 1
+
 
 class Player:
     """Creates a player object, a player object owns a GameBoard."""
@@ -83,7 +87,7 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.myBoard = GameBoard(5)
-        self.myBoard.place_ship(0,1,0,4)
+        self.myBoard.place_ship(0, 1, 0, 4)
 
 
 def main():
@@ -96,4 +100,3 @@ def main():
 
 
 main()
-
