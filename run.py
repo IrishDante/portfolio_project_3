becbef6e35f67abd16a5d62f1e815de150f7c557
Daniel Neville <dneville@tcd.ie>
2023-10-15 12:49:38 +0100

    added check_if_hit to check if a selected coordinate is a hit

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ MISS = "*"
 
 class GameBoard:
     """Creates a game board object"""
-    grid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    grid = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
     def __init__(self, board_dimension):
         """Initialises Game Board object based on dimensions provided taking board dimension allow dynamic boards"""
@@ -38,9 +38,8 @@ class GameBoard:
 
     def update_game_board(self, row, column, new_value):
         """Updates a value at a specific coordinate on the game board"""
-        row -= 1
-        column -= 1
-        self.grid[row + self.board_dimension * column] = new_value
+        coordinate = self.get_coordinate(row, column)
+        self.grid[coordinate] = new_value
 
     def place_ship(self, starting_row, starting_column, end_row, end_column):
         """
@@ -83,14 +82,42 @@ class GameBoard:
                 self.update_game_board(ship_row_long-ship_length, ship_column_long, SHIP)
                 ship_length -= 1
 
+    def check_if_legal_ship(self):
+        return None
+
+    def check_if_hit(self, row, column):
+        """
+        Takes coordinates and checks if that location is a HIT, MISS or has already been selected
+        updates location as appropriate
+        """
+        coordinate = self.get_coordinate(row, column)
+        if self.grid[coordinate] == SHIP:
+            self.grid[coordinate] = HIT
+            print(f"Hit at {row},{column}")
+            return "Hit"
+        elif self.grid[coordinate] == HIT or self.grid[coordinate] == MISS:
+            return "Already chosen"
+        elif self.grid[coordinate] == EMPTY:
+            self.grid[coordinate] = MISS
+            print(f"Miss at {row},{column}")
+            return "Miss"
+        else:
+            return "Error in coordinate provided"
+
+    def get_coordinate(self, row, column):
+        """Returns the location of the coordinate for [row][column] in the 1D list"""
+        row -= 1
+        column -= 1
+        return row + self.board_dimension * column
+
 
 class Player:
     """Creates a player object, a player object owns a GameBoard."""
 
     def __init__(self, name):
         self.name = name
-        self.myBoard = GameBoard(3)
-        self.myBoard.place_ship(3, 2, 1, 2)
+        self.myBoard = GameBoard(4)
+        self.myBoard.place_ship(1, 4, 1, 2)
 
 
 def main():
